b812be787b5f05c60efdb54c02e848dc9a76c76b
Basak Güleçoğlu <77547527+basakgulecoglu@users.noreply.github.com>
2023-03-12 15:08:05 +0300

    Add files via upload

---
+++ hw_4.sql
@@ -0,0 +1,10 @@
+--select distinct replacement_cost from film
+
+--select count (distinct replacement_cost) from film
+
+--select count(title) from film where title like 'T%' and
+--	rating='G'
+	
+--select count(*) from country where country like '_____'
+
+select count(city) from city where city like 'R%r'
