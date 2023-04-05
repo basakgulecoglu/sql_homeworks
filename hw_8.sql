5869a00aae6dddc48c62bd640d59f599278ea355
Basak Güleçoğlu <77547527+basakgulecoglu@users.noreply.github.com>
2023-04-05 20:58:27 +0300

    Add files via upload

---
+++ hw_8.sql
@@ -0,0 +1,25 @@
+/*create table employee (
+	id integer primary key,
+	name varchar(50) not null,
+	birthday date ,
+	email varchar(100));*/
+
+--select * from employee
+
+/*update employee set name='Adam', birthday='1999-12-02',
+	email='' where id=1 returning *;*/
+	
+/*update employee set name='Kamil', birthday='1989-02-02',
+	email='' where name='Hamil' returning *;*/
+	
+--delete from employee where id <4;
+
+--delete from employee where name='Adda';
+
+--delete from employee where birthday>'1990-12-31';
+
+--delete from employee WHERE email is Null;
+
+select * from employee	
+
+	
